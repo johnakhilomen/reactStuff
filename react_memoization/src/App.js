9c285c4b2e9a8156784601fdacafd96564550115
John Akhilomen <jonbasanti@gmail.com>
2023-01-19 20:12:56 -0600

    msg

--- react_memoization/src/App.js
+++ react_memoization/src/App.js
@@ -1,12 +1,13 @@
 import logo from "./logo.svg";
 import "./App.css";
 import ParentClassWithMemoization from "./ParentClassNoMemoization";
+import ParentClassNoMemoization from "./ParentClassNoMemoization";
 
 function App() {
   return (
     <div className="App">
-      {/* <ParentClassNoMemoization /> */}
-      <ParentClassWithMemoization />
+      <ParentClassNoMemoization />
+      {/* <ParentClassWithMemoization /> */}
     </div>
   );
 }
--- react_memoization/src/ChildClassNoMemoization.js
+++ react_memoization/src/ChildClassNoMemoization.js
@@ -1,6 +1,6 @@
-import React, { PureComponent } from "react";
+import React, { Component } from "react";
 
-class ChildClassWithMemoization extends PureComponent {
+class ChildClassWithMemoization extends Component {
   render() {
     console.log("Child render");
     return (
